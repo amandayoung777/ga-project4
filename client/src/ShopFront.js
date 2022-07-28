034ef9a5a5c599ffbc5910ad406bce61dcb91918
amandayoung777 <amanda.young777@gmail.com>
2022-07-28 20:57:57 +1000

    add

---
+++ client/src/ShopFront.js
@@ -0,0 +1,7 @@
+function ShopFront() {
+    return (
+        <h1 className="shopName">Amanda's Little Shop of Horrors</h1>
+    )
+}
+
+export default ShopFront
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const pg = require('pg')
 
-const port = 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 
 app.get('/', (req, res) => {
