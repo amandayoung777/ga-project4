bcded99219ab72b7296e65b16b4a9a430fd60603
amandayoung777 <amanda.young777@gmail.com>
2022-08-06 11:20:32 +1000

    updated server.js so heroku can be refreshed

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use((err, req, res, next) => {
 
 
 /* final catch-all route to index.html defined last */
-server.get('/*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(__dirname + '/client/build/index.html');
 })
 
