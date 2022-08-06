aa15195998da4361f72bf867715d1c86d6141d49
amandayoung777 <amanda.young777@gmail.com>
2022-08-06 13:48:04 +1000

    fixed a typo, whoops

--- client/src/components/AboutPage.js
+++ client/src/components/AboutPage.js
@@ -2,25 +2,28 @@ function AboutPage() {
   return (
     <div className="About">
       <p>Hi, I'm Manda and this is my baking shop.</p>
-      <p>I made this app as my final project for Generay Assembly. </p>
+      <p>I made this app as my final project for General Assembly. </p>
       <p>
         I learned a lot about React doing this. I also learnt a lot about
-        myself.{" "}
+        myself.
       </p>
       <p>I learnt that you should plan from the BEGINNING and not as you go.</p>
+
       <p>
-        I learnt that it would be better to think about the MVP and how you{" "}
+        I learnt that it would be better to think about the MVP and how you can
+        meet that before you start working on 7 other functions that you
+        inevitably will not complete in the timeframe given.
       </p>
-      <p>can meet that before you start working on 7 other functions </p>
-      <p> that you inevitably will not complete in the timeframe given.</p>
-      <p>I learnt that when you're on the verge of giving up altogether, </p>
       <p>
-        there's nobody who understands the struggle better than your classmates,
-        so{" "}
+        I learnt that when you're on the verge of giving up altogether, there's
+        nobody who understands the struggle better than your classmates, so
+        shout out to everyone who listened to me rant whilst I made this!
       </p>
-      <p>shout out to everyone who listened to me rant whilst I made this!</p>
       <p></p>
-      <p>PS If you want to buy anything, you have to create an account and send me a message...</p>
+      <p>
+        PS If you want to buy anything, you have to create an account and send
+        me a message...
+      </p>
     </div>
   );
 }
